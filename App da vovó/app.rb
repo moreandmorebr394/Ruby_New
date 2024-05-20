a7f9d3a797b965378a1f32ea9baf3cb5939b2a24
Bruna Alve <130995251+moreandmorebr394@users.noreply.github.com>
2024-05-20 09:40:28 -0300

    Update app.rb

--- App da vovó/app.rb
+++ App da vovó/app.rb
@@ -2,7 +2,7 @@ require_relative 'produto'
 require_relative 'loja'
 
 produto = Produto.new
-  produto.nome = "Forma para bolo redondo"
-  produto.preco = "36.00"
+  produto.nome = "Tomate"
+  produto.preco = "3.00"
 
-Loja.new(produto.nome, produto.preco).comprar
+Mercado.new(produto.nome, produto.preco).comprar
